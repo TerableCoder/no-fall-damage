e9bda1c37b3b1318d85f84cc97040349789fff99
Kasea <a1ex_john@hotmail.com>
2017-11-07 02:27:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 module.exports = function drop(dispatch){
         const command = require('command')(dispatch);
         var enabled = true;
+        var currZone;
         
         command.add('fall', ()=>{
                 enabled = !enabled;
                 command.message("Fall damage has been " + (enabled?"enabled.":"disabled."));
         });
         
+        dispatch.hook('S_LOAD_TOPO', 2, e=>{
+                currZone = e.zone;
+        });
+        
         dispatch.hook('C_PLAYER_LOCATION', 1, e=>{
-                return !([2, 10].includes(e.type));       
+                return !([2, 10].includes(e.type) && (currZone < 10 || currZone > 200));       
         });
 }
