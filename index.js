389eb5336de41f8a437ee9771ed3c0ea26e1fb7e
Fatvallon <43625745+Fatvallon@users.noreply.github.com>
2018-10-25 21:56:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ module.exports = function drop(dispatch){
                 command.message("Fall damage has been " + (enabled?"enabled.":"disabled."));
         });
         
-        dispatch.hook('S_LOAD_TOPO', 2, e=>{
+        dispatch.hook('S_LOAD_TOPO', 3, e=>{
                 currZone = e.zone;
         });
         
-        dispatch.hook('C_PLAYER_LOCATION', 1, e=>{
+        dispatch.hook('C_PLAYER_LOCATION', 5, e=>{
                 return !([2, 10].includes(e.type) && (currZone < 10 || currZone > 200));       
         });
 }
