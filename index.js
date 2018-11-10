2572d5040e4761c9f6943b2ec24556da6c4b14c4
Fatvallon <43625745+Fatvallon@users.noreply.github.com>
2018-11-10 16:02:35 +0200

    (dispatch) => (mod)

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-module.exports = function drop(dispatch){
+module.exports = function drop(mod){
         var enabled = true;
         var currZone;
         
-        dispatch.command.add('fall', ()=>{
+        mod.command.add('fall', ()=>{
                 enabled = !enabled;
-                dispatch.command.message("Fall damage has been " + (enabled?"enabled.":"disabled."));
+                mod.command.message("Fall damage has been " + (enabled?"enabled.":"disabled."));
         });
         
-        dispatch.hook('S_LOAD_TOPO', 3, e=>{
+        mod.hook('S_LOAD_TOPO', 3, e=>{
                 currZone = e.zone;
         });
         
-        dispatch.hook('C_PLAYER_LOCATION', 5, e=>{
+        mod.hook('C_PLAYER_LOCATION', 5, e=>{
                 return !([2, 10].includes(e.type) && (currZone < 10 || currZone > 200));       
         });
 }
